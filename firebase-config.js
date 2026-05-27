ce68637876aa1b3d3cc5260fd0102831a691c10e
Ureaka <Ureaka@users.noreply.github.com>
2026-05-27 23:03:11 +0000

    deploy: 9b4107489a7f03c12ba19a9e19005ca3fc799486

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const FIREBASE = {
+  apiKey:            "AIzaSyBWNm2d8td5moMe8ThjEiuo55m1XggtPe0",
+  authDomain:        "ship-checker-46ab1.firebaseapp.com",
+  projectId:         "ship-checker-46ab1",
+  storageBucket:     "ship-checker-46ab1.appspot.com",
+  messagingSenderId: "545444841024",
+  appId:             "1:545444841024:web:13f26edbc0f745933501a5"
+};
+const GEMINI_API_KEY = "AIzaSyA26Dnc52kPPh1M5DTDleeEpPwK3GwjW1k";
